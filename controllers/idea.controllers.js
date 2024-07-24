896763609de84286d95789e6798103494444cfab
Aryan Kumar Shrivastav <main.aryanshrivastav2003@gmail.com>
2024-07-24 11:59:22 +0530

    Get idea based on id

--- controllers/idea.controllers.js
+++ controllers/idea.controllers.js
@@ -19,3 +19,28 @@ exports.getAllIdeas= (req,res)=>{
     // Return all those ideas
     res.status(200).send(ideas)
 }
+
+/**
+ * Controller that fetches idea based on their id
+ * 
+ * GET 127.0.0.1/5000/ideaApp/api/v1/ideas/2
+ */
+
+exports.getIdeasBasedonId=(req,res,next)=>{
+       //We need to first read the idea id from req path param 
+    idea_id=req.params.id
+    
+      //Using id check whether it is present
+    if(ideas[idea_id]){
+        res.status(200).send(ideas[idea_id])
+    }
+    else{
+        res.status(404).send({
+            message : "idea not found for the given id" 
+        })
+    }
+     
+      //if present then return id 
+ 
+
+}
--- routes/idea.routes.js
+++ routes/idea.routes.js
@@ -8,4 +8,7 @@ const idea_controllers=require("../controllers/idea.controllers")
 
 module.exports=(app)=>{
     app.get("/ideaApp/api/v1/ideas",idea_controllers.getAllIdeas)
+
+    // Route for ideaApp/api/v1/ideas/:id
+    app.get("/ideaApp/api/v1/ideas/:id",idea_controllers.getIdeasBasedonId)
 }
